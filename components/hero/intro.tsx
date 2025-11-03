e4363df7bcad4247c1a59ed54c177e908f6dd869
Hikaru <imhikaru@proton.me>
2025-11-03 12:53:55 +0800

    style: Fine-tune line spacing

--- components/hero/intro.tsx
+++ components/hero/intro.tsx
@@ -21,7 +21,7 @@ export function Intro() {
             打破常规。
           </Text>
           <Stack gap={{ base: 4, md: 5 }} fontSize={{ base: "md", md: "xl" }} color={bodyColor}>
-            <Text lineHeight={1.8}>
+            <Text lineHeight={1.6}>
               DailyNotes 天生就与众不同。手表行业首创手帐类编辑软件，实现类富文本第一人。强大的手帐编辑器人人都能快速上手，
               除此之外，还有丰富的个性化资源，
               <Text as="span" fontWeight="semibold" color={highlight}>
@@ -29,7 +29,7 @@ export function Intro() {
               </Text>
               。
             </Text>
-            <Text lineHeight={1.8}>
+            <Text lineHeight={1.6}>
               手帐内不仅支持导入
               <Text as="span" fontWeight="semibold" color={highlight}>
                 {" "}图片、视频、录音
